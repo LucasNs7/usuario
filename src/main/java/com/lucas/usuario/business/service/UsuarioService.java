cf92b177b558f8db0221dd6741a4d00aaf6718bd
Lucas Sousa <lucas.sousadev07@gmail.com>
2026-05-15 11:03:20 -0300

    feat: add ServiceHelper and refactor UsuarioService

--- src/main/java/com/lucas/usuario/business/service/UsuarioService.java
+++ src/main/java/com/lucas/usuario/business/service/UsuarioService.java
@@ -1,9 +1,9 @@
 package com.lucas.usuario.business.service;
 
+import com.lucas.usuario.business.Helper.ServiceHelper;
 import com.lucas.usuario.business.converter.UsuarioConverter;
 import com.lucas.usuario.business.dto.UsuarioDTO;
 import com.lucas.usuario.infrastructure.entity.Usuario;
-import com.lucas.usuario.infrastructure.exceptions.ConflictException;
 import com.lucas.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
@@ -16,31 +16,16 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final ServiceHelper serviceHelper;
     private final PasswordEncoder encoder;
 
     public UsuarioDTO criaUsuario(@NonNull UsuarioDTO usuarioDTO) {
-        existsEmail(usuarioDTO.getEmail());
+        serviceHelper.existsEmail(usuarioDTO.getEmail());
 
         usuarioDTO.setSenha(encoder.encode(usuarioDTO.getSenha()));
 
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
 
-        return usuarioConverter.paraUsuarioDTO(
-                usuarioRepository.save(usuario)
-        );
-    }
-
-    public void existsEmail(String email) {
-        try {
-            boolean existe = verificaEmailExistente(email);
-
-            if (existe) {
-                throw new ConflictException("Email: " + email + " já cadastrado!");
-            }
-        } catch (Exception e) { throw new ConflictException("Email existente!", e.getCause()); }
-    }
-
-    public boolean verificaEmailExistente(String email) {
-        return usuarioRepository.existsByEmail(email);
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
     }
 }
